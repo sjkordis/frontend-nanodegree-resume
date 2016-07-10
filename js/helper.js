150d8962477e1bfbcbcbab3788e3bfa33da1162c
sukor <susan.kordis@hp.com>
2016-07-09 19:10:10 -0600

    Completed first reviewable version of the project; submitting for feedback

--- js/helper.js
+++ js/helper.js
@@ -6,8 +6,6 @@ Don't worry, you'll learn what's going on in this file throughout the course. Yo
 
 Cameron Pittman
 */
-
-
 /*
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
@@ -43,60 +41,26 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href=http://www.%url%>%name%';
 var HTMLschoolDegree = '&nbsp;&ndash;&nbsp;%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineClasses = '<h3 id="online-h3">Online Classes</h3>';
+var HTMLonlineTitle = '<a href="http://www.%url%">%title%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
+var HTMLonlineUrl = '<a class="url-text" href="http://www.%url%">%text%</a><br>';
 
-var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
-
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
-    $name.html(iName);
-  });
-});
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
+var map; // declares a global map variable
 
 
 /*
@@ -133,7 +97,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    education.schools.forEach(function(school) {
       locations.push(school.location);
     });
 
@@ -141,7 +105,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.jobs.forEach(function(job) {
       locations.push(job.location);
     });
 
@@ -156,10 +120,10 @@ function initializeMap() {
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat(); // latitude from the place service
+    var lon = placeData.geometry.location.lng(); // longitude from the place service
+    var name = placeData.formatted_address; // name of the place from the place service
+    var bounds = window.mapBounds; // current boundaries of the map window
 
     // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
@@ -210,7 +174,7 @@ function initializeMap() {
     var service = new google.maps.places.PlacesService(map);
 
     // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
+    locations.forEach(function(place) {
       // the search request object
       var request = {
         query: place
@@ -245,5 +209,5 @@ window.addEventListener('load', initializeMap);
 // and adjust map bounds
 window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
- map.fitBounds(mapBounds);
+  map.fitBounds(mapBounds);
 });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,128 +1,28 @@
-// begin data //
+/*
+Biographical information for the resume
+*/
 
 var bio = {
-    "name" : "Susan Kordis",
-    "role" : "API Documentation Specialist",
-    "contacts" : {
-        "email" : "susankordis@gmail.com",
-        "mobile" : "970-402-7390",
-        "github" : "sjkordis",
-        "twitter" : "@susan_kordis",
-        "location" : "Colorado"
+    "name": "Susan Kordis",
+    "role": "API Documentation Specialist",
+    "contacts": {
+        "email": "susankordis@gmail.com",
+        "mobile": "970-402-7390",
+        "github": "sjkordis",
+        "twitter": "@susan_kordis",
+        "location": "Colorado"
     },
-    "pictureURL" : "images/susan.jpg",
-    "welcomeMessage" : "APIs make the world go round!",
-    "skills" : ["API documentation", "Front-end web development", "Agile development", "GitBook", "Swagger", "Markdown", "MadCap Flare"]
-}
+    "biopic": "images/susan.jpg",
+    "welcomeMessage": "APIs make the world go round!",
+    "skills": ["API documentation", "Front-end web development", "Agile development", "GitBook", "Swagger", "Markdown", "MadCap Flare"]
+};
 
-var work = {
-    "jobs": [
-        {
-            "employer": "HP Chief Technology Office",
-            "title": "Information Engineering Team Lead",
-            "location": "Fort Collins, Colorado",
-            "dates": "2013-present",
-            "description": "Documented APIs for web services and created documentation portals"
-        },
-        {
-            "employer": "HP Software",
-            "title": "Information Engineer",
-            "location": "Fort Collins, Colorado",
-            "dates": "2007-2013",
-            "description": "Documented APIs for web services and created admin guides"
-        },
-        {
-            "employer": "KForce",
-            "title": "Technical Writer",
-            "location": "Fort Collins, Colorado",
-            "dates": "2002-2007",
-            "description": "Created and maintained user and administrator documentation for several HP Software products"
-        },
-        {
-            "employer": "Change Technologies",
-            "title": "Information Developer",
-            "location": "Fort Collins, Colorado",
-            "dates": "1991-2001",
-            "description": "Created and maintained marketing literature, training instruments, and instructional books"
-        },
-        {
-            "employer": "HP - Colorado Integrated Circuits Division",
-            "title": "Digital Hardware Engineer",
-            "location": "Fort Collins, Colorado",
-            "dates": "1984-1990",
-            "description": "Created automated test software for a proprietary integrated circuit tester; supported internal customers in bringing integrated circuit designs to production"
-        }
-    ]
-}
+/**
+* @description Used to display the contact information from the bio object. This information occurs both in the header and the footer of the resume, so the location is passed as a parameter.
+* @param {string} locationId - The HTML <ul> ID where the contact information is to be displayed
+*/
 
-var education = {
-    "schools": [
-        {
-            "name": "Udacity.com",
-            "city": "Online",
-            "grad": 2016,
-            "degree": "Nanodegree",
-            "major": "Front-End Web Development"
-        },
-        {
-            "name": "New York Institute of Technology",
-            "city": "New York, NY",
-            "grad": 2008,
-            "degree": "MBA",
-            "major": "Economic Strategy and Online Marketing"
-        },
-        {
-            "name": "University of Wisconsin",
-            "city": "Madison, WI",
-            "grad": 1984,
-            "degree": "BS",
-            "major": "Electrical and Computer Engineering"
-        }
-    ]
-}
-
-var projects = {
-    "projects": [
-        {
-            "title": "HP Common Web Platform",
-            "dates": "2013-present",
-            "description": "Created documentation for a platform of fundamental web services that HP web applications consume",
-            "images": [
-                "images/cwp1.jpg", "images/cwp2.jpg", "images/cwp3.jpg"
-            ]
-        },
-        {
-            "title": "HP Database and Middleware Automation",
-            "dates": "2011-2013",
-            "description": "Created user guides, admin guides, online help, and API documentation",
-            "images": [
-                "images/dma1.jpg", "images/dma2.jpg", "images/dma3.jpg"
-            ]
-        },
-        {
-            "title": "HP Server Automation",
-            "dates": "2009-2011",
-            "description": "Created user guides, admin guides, and online help",
-            "images": [
-                "images/sa1.jpg", "images/sa2.jpg", "images/sa3.jpg"
-            ]
-        },
-        {
-            "title": "HP Enterprise Discovery",
-            "dates": "2007-2009",
-            "description": "Created user guides, admin guides, and online help",
-            "images": [
-                "images/sa1.jpg", "images/sa2.jpg", "images/sa3.jpg"
-            ]
-        }
-    ]
-}
-
-// end of data
-
-// begin functions
-
-function displayContacts (locationId){
+bio.displayContacts = function(locationId) {
     var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
     var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
     var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
@@ -136,89 +36,240 @@ function displayContacts (locationId){
     $(locationId).append(formattedLocation);
 }
 
-function displayHeader() {
+/**
+* @description Used to display everything that comes before the list of skills.
+*/
+
+bio.displayHeader = function() {
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
     var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-    var bioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+    var bioPic = HTMLbioPic.replace("%data%", bio.biopic);
 
     $("#header").prepend(formattedRole);
     $("#header").prepend(formattedName);
-    displayContacts("#topContacts");
+    bio.displayContacts("#topContacts");
     $("#header").append(bioPic);
     $("#header").append(formattedMsg);
-
 }
 
-function displaySkills () {
-    $("#header").append (HTMLskillsStart);
-    for ( skill in bio.skills) {
-        var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+/**
+* @description Used to display the list of skills.
+*/
+
+bio.displaySkills = function() {
+    $("#header").append(HTMLskillsStart);
+    for (var indexCount = 0; indexCount < bio.skills.length; indexCount++) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[indexCount]);
         $("#skills").append(formattedSkills);
     }
 }
 
-function displayWork () {
-    for ( job in work.jobs) {
+/**
+* @description Displays everything in the header of the resume.
+*/
+
+bio.display = function() {
+    bio.displayHeader();
+    bio.displaySkills();
+}
+
+/*
+Work history information organized in reverse chronological order.
+*/
+
+var work = {
+    "jobs": [{
+        "employer": "HP Chief Technology Office",
+        "title": "Information Engineering Team Lead",
+        "location": "Fort Collins, Colorado",
+        "dates": "2013-present",
+        "description": "Documented APIs for web services and created documentation portals"
+    }, {
+        "employer": "HP Software",
+        "title": "Information Engineer",
+        "location": "Sunnyvale, CA",
+        "dates": "2007-2013",
+        "description": "Documented APIs for web services and created admin guides"
+    }, {
+        "employer": "KForce",
+        "title": "Technical Writer",
+        "location": "Corvallis, OR",
+        "dates": "2002-2007",
+        "description": "Created and maintained user and administrator documentation for several HP Software products"
+    }, {
+        "employer": "Change Technologies",
+        "title": "Information Developer",
+        "location": "Minneapolis, MN",
+        "dates": "1991-2001",
+        "description": "Created and maintained marketing literature, training instruments, and instructional books"
+    }, {
+        "employer": "HP - Colorado Integrated Circuits Division",
+        "title": "Digital Hardware Engineer",
+        "location": "Fort Collins, Colorado",
+        "dates": "1984-1990",
+        "description": "Created automated test software for a proprietary integrated circuit tester; supported internal customers in bringing integrated circuit designs to production"
+    }]
+};
+
+/**
+* @description Displays each job in the work history.
+*/
+
+work.display = function() {
+    for (var indexCount = 0; indexCount < work.jobs.length; indexCount++) {
         $("#workExperience").append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[indexCount].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[indexCount].title);
         var formattedEmployerTitle = formattedEmployer + formattedTitle;
         $(".work-entry:last").append(formattedEmployerTitle);
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[indexCount].dates);
         $(".work-entry:last").append(formattedDates);
-        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[indexCount].location);
         $(".work-entry:last").append(formattedLocation);
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[indexCount].description);
         $(".work-entry:last").append(formattedDescription);
     }
 }
 
+/*
+List of specific projects in reverse chronological order.
+*/
+
+var projects = {
+    "projects": [{
+        "title": "HP Common Web Platform",
+        "dates": "2013-present",
+        "description": "Created documentation for a platform of fundamental web services that HP web applications consume",
+        "images": [
+            "images/platform.jpg"
+        ]
+    }, {
+        "title": "HP Database and Middleware Automation",
+        "dates": "2011-2013",
+        "description": "Created user guides, admin guides, online help, and API documentation",
+        "images": [
+            "images/dbauto.jpg"
+        ]
+    }, {
+        "title": "HP Server Automation",
+        "dates": "2009-2011",
+        "description": "Created user guides, admin guides, and online help",
+        "images": [
+            "images/sa.jpg"
+        ]
+    }, {
+        "title": "HP Enterprise Discovery",
+        "dates": "2007-2009",
+        "description": "Created user guides, admin guides, and online help",
+        "images": [
+            "images/entdisc.jpg"
+        ]
+    }]
+};
+
+/**
+* @description Displays each project in the list of projects, including any images associated with each project.
+*/
+
 projects.display = function() {
-    for ( project in this.projects ) {
+    for (var indexCount = 0; indexCount < projects.projects.length; indexCount++) {
         $("#projects").append(HTMLprojectStart);
-        var formattedTitle = HTMLprojectTitle.replace("%data%", this.projects[project].title);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[indexCount].title);
         $(".project-entry:last").append(formattedTitle);
 
-        var formattedDates = HTMLprojectDates.replace("%data%", this.projects[project].dates);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[indexCount].dates);
         $(".project-entry:last").append(formattedDates);
 
-        var formattedDescription = HTMLprojectDescription.replace("%data%", this.projects[project].description);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[indexCount].description);
         $(".project-entry:last").append(formattedDescription);
 
-        for (image in projects.projects[project].images) {
-            var formattedImage = HTMLprojectImage.replace("%data%", this.projects[project].images[image]);
+        for (var imageCount = 0; imageCount < projects.projects[indexCount].images.length; imageCount++) {
+            var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[indexCount].images[imageCount]);
             $(".project-entry:last").append(formattedImage);
         }
     }
 }
 
+/*
+Degrees attained and online courses completed, in reverse chronological order
+*/
+
+var education = {
+    "schools": [{
+        "name": "New York Institute of Technology",
+        "location": "New York, NY",
+        "dates": "2006-2008",
+        "degree": "MBA",
+        "majors": "Economic Strategy and eCommerce",
+        "url": "nyit.edu"
+    }, {
+        "name": "University of Wisconsin",
+        "location": "Madison, WI",
+        "dates": "1980-1984",
+        "degree": "BS",
+        "majors": "Electrical and Computer Engineering",
+        "url": "wisc.edu"
+    }],
+    "onlineCourses": [{
+        "title": "Front-End Web Development Nanodegree",
+        "school": "Udacity",
+        "dates": "2016",
+        "url": "udacity.com"
+    }, {
+        "title": "Up and Running with Bootstrap 3.0",
+        "school": "Lynda",
+        "dates": "2015",
+        "url": "lynda.com"
+    }, {
+        "title": "Up and Running with Python",
+        "school": "Lynda",
+        "dates": "2015",
+        "url": "lynda.com"
+    }, ]
+};
+
+/**
+* @description Displays educational information, including degrees earned and online courses completed.
+*/
+
 education.display = function() {
-    for ( school in this.schools ) {
+    for (var indexCount = 0; indexCount < this.schools.length; indexCount++) {
         $("#education").append(HTMLschoolStart);
-        var formattedName = HTMLschoolName.replace("%data%", this.schools[school].name);
-        var formattedDegree = HTMLschoolDegree.replace("%data%", this.schools[school].degree);
+        var formattedName = HTMLschoolName.replace("%name%", this.schools[indexCount].name);
+        formattedName = formattedName.replace("%url%", this.schools[indexCount].url);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", this.schools[indexCount].degree);
         var formattedNameDegree = formattedName + formattedDegree;
         $(".education-entry:last").append(formattedNameDegree);
-        var formattedDates = HTMLschoolDates.replace("%data%", this.schools[school].grad);
+        var formattedDates = HTMLschoolDates.replace("%data%", this.schools[indexCount].dates);
         $(".education-entry:last").append(formattedDates);
-        var formattedLocation = HTMLschoolLocation.replace("%data%", this.schools[school].city);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", this.schools[indexCount].location);
         $(".education-entry:last").append(formattedLocation);
-        var formattedMajor = HTMLschoolMajor.replace("%data%", this.schools[school].major);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", this.schools[indexCount].majors);
         $(".education-entry:last").append(formattedMajor);
+    }
+    $(".education-entry:last").append(HTMLonlineClasses);
+    for (indexCount = 0; indexCount < this.onlineCourses.length; indexCount++) {
+        var formattedTitle = HTMLonlineTitle.replace("%title%", this.onlineCourses[indexCount].title);
+        formattedTitle = formattedTitle.replace("%url%", this.onlineCourses[indexCount].url);
+        var formattedSchool = HTMLonlineSchool.replace("%data%", this.onlineCourses[indexCount].school);
+        var formattedTitleSchool = formattedTitle + formattedSchool;
+        $(".education-entry:last").append(formattedTitleSchool);
+        var formattedDate = HTMLonlineDates.replace("%data%", this.onlineCourses[indexCount].dates);
+        $(".education-entry:last").append(formattedDate);
+        var formattedUrl = HTMLonlineUrl.replace("%url%", this.onlineCourses[indexCount].url);
+        formattedUrl = formattedUrl.replace("%text%", this.onlineCourses[indexCount].url);
+        $(".education-entry:last").append(formattedUrl);
     }
 }
 
-// end of functions
-
 // begin main
 
-displayHeader();
-displaySkills();
-displayWork();
+bio.display();
+work.display();
 projects.display();
 education.display();
-displayContacts( "#footerContacts");
+$("#mapDiv").append(googleMap);
+bio.displayContacts("#footerContacts");
 
 // end main
-
